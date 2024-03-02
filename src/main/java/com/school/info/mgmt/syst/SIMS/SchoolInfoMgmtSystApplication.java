eff6f46509b16a79b40c62a63f8d644b09530439
DevCommonResource <devcommonresource@gmail.com>
2024-03-02 10:22:26 +0530

    initial commit

--- src/main/java/com/school/info/mgmt/syst/SIMS/SchoolInfoMgmtSystApplication.java
+++ src/main/java/com/school/info/mgmt/syst/SIMS/SchoolInfoMgmtSystApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchoolInfoMgmtSystApplication {
 
 	public static void main(String[] args) {
+		System.out.println("SchoolInfoMgmtSystApplication Running...");
 		SpringApplication.run(SchoolInfoMgmtSystApplication.class, args);
 	}
 
