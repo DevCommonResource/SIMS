ece98f2df76f0c4a650270b70e3f77022bc6b482
DevCommonResource <161912018+DevCommonResource@users.noreply.github.com>
2024-03-02 10:14:54 +0530

    initial commit

---
+++ src/main/java/com/school/info/mgmt/syst/SIMS/SchoolInfoMgmtSystApplication.java
@@ -0,0 +1,13 @@
+package com.school.info.mgmt.syst.SIMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolInfoMgmtSystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolInfoMgmtSystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/info/mgmt/syst/SIMS/SchoolInfoMgmtSystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.info.mgmt.syst.SIMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolInfoMgmtSystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
